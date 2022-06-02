c1c4fbad50ec230c463062c55782175dd9274a39
Goke969 <71637901+Goke969@users.noreply.github.com>
2022-06-02 13:17:19 +0100

    First web project

---
+++ CaineSocial.js
@@ -0,0 +1,6 @@
+document.querySelector(".hamburger-menu").addEventListener("click", ()=>{
+document.querySelector(".container").classList.toggle("change");
+});
+document.querySelector(".photography").addEventListener("scroll", ()=>{
+    document.querySelector(".container").classList.toggle("change");
+    });
